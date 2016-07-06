a750637d064e99ea508c74cff427dc4e0e7cc525
Dr762 <abondar1992@gmail.com>
2016-07-06 22:54:21 +0300

    Base for jdbc demo

---
+++ SpringDataBase/src/main/java/org/abondar/experimental/jdbc/ContactDao.java
@@ -0,0 +1,17 @@
+package org.abondar.experimental.jdbc;
+
+import java.util.List;
+
+/**
+ * Created by abondar on 06.07.16.
+ */
+public interface ContactDao {
+    List<Contact> findAll();
+    List<Contact> findByFirstName(String firstName);
+    String findLastNameById(Long id);
+    String findFirstNameById(Long id);
+    void insert(Contact contact);
+    void update (Contact contact);
+    void delete(Long contactId);
+
+}
