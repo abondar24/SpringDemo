80bd060b1a35dfda5c4465aa08b2dbe973c82269
abondar24 <abondar1992@gmail.com>
2017-03-09 15:51:27 -0800

    Spring Boot fix

--- SpringBoot/src/main/java/org/abondar/experimental/springboot/Application.java
+++ SpringBoot/src/main/java/org/abondar/experimental/springboot/Application.java
@@ -3,11 +3,13 @@ package org.abondar.experimental.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by abondar on 25.07.16.
  */
 
+@Configuration
 @SpringBootApplication
 @EnableAutoConfiguration
 public class Application {
